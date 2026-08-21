f8638922fb35987ba5fe25c7e4dbcaf4c749f5ad
Hope-dev90 <mutimutujehope90@gmail.com>
2026-08-21 20:04:55 +0100

    first commit

---
+++ auth/src/test/java/com/hope/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hope.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
